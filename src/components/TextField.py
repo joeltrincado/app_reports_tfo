a59d0ddb057667fffaa418746565df4346264c48
joeltrincado <joetectronic@gmail.com>
2025-03-21 10:22:02 -0600

    cambios de posicion y entrada de texto

--- src/components/TextField.py
+++ src/components/TextField.py
@@ -1,10 +1,11 @@
 import flet as ft
 
 class TextField_():
-    def __init__(self, text, w, onchange):
+    def __init__(self, text, w, onchange, on_summit):
         self.text = text
         self.w = w
         self.onchange = onchange
+        self.onsummit = on_summit
 
     def create(self):
-        return ft.TextField(width=self.w,label=self.text, border_color=ft.colors.BLACK54,expand=True, on_change=lambda e: self.onchange(e))
+        return ft.TextField(width=self.w,label=self.text, border_color=ft.colors.BLACK54,expand=True, on_change=lambda e: self.onchange(e), on_submit=lambda e: self.onsummit(e))
